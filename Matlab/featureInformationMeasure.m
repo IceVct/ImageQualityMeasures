3cf40c383bcf739f83b9f65d814cf8c6ff78840b
icevct <icevct@gmail.com>
2018-12-12 12:11:56 -0200

    both occlusion and dilation measures done, doing information measure

--- Matlab/featureInformationMeasure.m
+++ Matlab/featureInformationMeasure.m
@@ -1,4 +1,4 @@
-function [fimMeasure] = featureInformationMeasure(normImage, normMaskImage, L, N)
+function [fimMeasure] = featureInformationMeasure(logGaborNormImage, normMaskImage, L, N)
 %featureInformationMeasure Function that computes the feature information
 %measures based on the iris normalized segmeneted image
 
@@ -6,15 +6,51 @@ function [fimMeasure] = featureInformationMeasure(normImage, normMaskImage, L, N
 % Image-Quality Measure (by Craig Belcher and Yingzi Du) nomenclature.
 
 %% Variables initialization
-[rows, cols] = size(normImage);
+[rows, cols] = size(logGaborNormImage);
 
 % information distance weighting variables
 k = 1/33;
 l = 0.1;
 Beta = 0.04;
 alpha = 1/Beta;
+sumConsecNonOcclud = zeros(rows, 1);
+r = zeros(rows, 1);
+p = zeros(rows, 1);
+q = zeros(rows, 1);
+wJ = zeros(rows, 1);
+Tmax = 1;
+Tmean = 1;
 
 %% Computing the measure
+
+% computing the sum of consecutive nonoccluded in every row of the image
+for i = 1:rows
+    sumConsecNonOcclud(i, 1) = max( diff( [0 (find( ~ (normMaskImage(i, :) > 0) ) ) numel(normMaskImage(i, :)) + 1] ) - 1);
+end
+
+% looping through the image rows and computing the information measures
+for i = 1:rows
+    for win = 1:L:cols
+        % computing the row maximum value and mean
+        maxValue = max(logGaborNormImage(i, :));
+        meanValue = mean(logGaborNormImage(i, :));
+        
+        % if the consecutive nonoccluded in a row is less than the parameter L, then the information
+        % distance for the row is set to 0
+        if(sumConsecNonOcclud(i) >= L)
+            r = double(logGaborNormImage(i, win:(win + L - 1)));
+            p = r/norm(r, 2); % computing the probability mass function of the vector r
+            q(:) = 1/L; % computing the uniform distribution of the vector
+            
+            % information measure
+            J = relativeEntropy(p, q) + relativeEntropy(q, p); 
+            wJ(i) = (1 - k*exp(l*i))*J; % weighting the clarity of iris patterns in the inner circles
+        else
+            wJ(i) = 0;
+        end
+        
+    end
+end
 
 %% Computing the measure normalization
 
---
+++ Matlab/relativeEntropy.m
@@ -0,0 +1,10 @@
+function [relEntropy] = relativeEntropy(inp1, inp2)
+%relativeEntropy Computes the relative entropy of two signals
+
+% Computing the relatives entropies
+relativeEntropies = inp1.*log2(inp1./inp2);
+
+relEntropy = sum(relativeEntropies);
+
+end
+
