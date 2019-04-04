4b152e5143a2d536cd115a3cc57d46a03adacb78
Victor <icevct@gmail.com>
2019-04-04 10:06:44 -0300

    dsmi threshold script done

--- Scripts/dsmi_threshold.py
+++ Scripts/dsmi_threshold.py
@@ -6,35 +6,47 @@ from subprocess import check_output
 
 # Script for computing the DSMI metric threshold, using the databases training set
 
+# CODE ADAPTATED FROM A STACK OVERFLOW ONE
+# https://stackoverflow.com/questions/48213278/implementing-otsu-binarization-from-scratch-python
+def otsu(array, nbins):
+    n_array = len(array)
+    mean_weigth = 1.0/n_array
+    his, bins = np.histogram(array, 100)
+    print bins
+    final_thresh = -1
+    final_value = -1
+    for t, bin in enumerate(bins[1:-1]): # This goes from 1 to 254 uint8 range (Pretty sure wont be those values)
+        Wb = np.sum(his[:t]) * mean_weigth
+        Wf = np.sum(his[t:]) * mean_weigth
+
+        mub = np.mean(his[:t])
+        muf = np.mean(his[t:])
+
+        value = Wb * Wf * (mub - muf) ** 2
+
+        # print("Wb", Wb, "Wf", Wf)
+        # print("t", t, "value", value)
+
+        if value > final_value:
+            final_thresh = bin
+            final_value = value
+    
+    return final_thresh
+
 def main():
     amount_arguments = len(sys.argv)
     # checking if the amount of valid arguments is being passed
-    if amount_arguments < 2 or amount_arguments > 4:
-        raise ValueError('Only 3 arguments must be passed!')
+    if amount_arguments < 2 or amount_arguments > 2:
+        raise ValueError('Only 1 argument must be passed!')
 
-    circle_params_file = sys.argv[1]
-    mask_file = sys.argv[2]
-    normalized_images_file = sys.argv[3]
+    # reading the file
+    with open(sys.argv[1], 'r') as f:
+        dsmi_values = f.read().splitlines()
 
-    # reading the files
-    with open(circle_params_file, 'r') as f:
-        circle_params = f.read().splitlines()
+    dsmi_values = [float(dsmi) for dsmi in dsmi_values]
 
-    with open(mask_file, 'r') as f:
-        masks = f.read().splitlines()
-        
-    with open(normalized_images_file, 'r') as f:
-        norm_images = f.read().splitlines()
-
-    computed_fcm = []
-    for i in range(0, len(norm_images)):
-        executable = ['python', '/home/vavieira/UnB/TCC/Codigos/ImageQualityMeasures/FeatureCorrelation/main.py', norm_images[i], masks[i], circle_params[i]]
-        fcm =  check_output(executable)
-        computed_fcm.append(float(fcm.rstrip()))
-    
-
-    mean = np.mean(computed_fcm)
-    print str(mean)
+    threshold = otsu(dsmi_values, 100)
+    print str(threshold)
 
 
 if __name__ == "__main__":
