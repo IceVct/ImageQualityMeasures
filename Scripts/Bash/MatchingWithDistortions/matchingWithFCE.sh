fe19b9948fdb96a39abb129c06e6818cbad63431
Victor Araujo Vieira <icevct@gmail.com>
2019-05-01 22:45:11 -0300

    Updating matching with FCE

--- Scripts/Bash/MatchingWithDistortions/matchingWithFCE.sh
+++ Scripts/Bash/MatchingWithDistortions/matchingWithFCE.sh
@@ -5,29 +5,30 @@
 
 ################################GENERATING FILES################################################################# 
 #Generating files with all the files that has a FCE value higher than the computed threshold = 0.7158462545478076 (0.71)
-
+echo "Generating files"
 # warsaw
-python ../Python/compare_file_measures.py ~/UnB/TCC/IrisDatabases/Warsaw-BioBase-Smartphone-Iris-v1.0/warsaw_test_fce_stats.txt /home/vavieira/UnB/TCC/IrisDatabases/Warsaw-BioBase-Smartphone-Iris-v1.0/warsaw_test_fce.txt 0.71
+python ../../Python/compare_file_measures.py ~/UnB/TCC/IrisDatabases/Warsaw-BioBase-Smartphone-Iris-v1.0/DistortionFiles/warsaw_test_distortions_fce_stats.txt /home/vavieira/UnB/TCC/IrisDatabases/Warsaw-BioBase-Smartphone-Iris-v1.0/DistortionFiles/warsaw_test_distortions_fce.txt 0.71
 
 # miche
-python ../Python/compare_file_measures.py ~/UnB/TCC/IrisDatabases/MICHE/miche_test_fce_stats.txt /home/vavieira/UnB/TCC/IrisDatabases/MICHE/miche_test_fce.txt 0.71
+python ../../Python/compare_file_measures.py ~/UnB/TCC/IrisDatabases/MICHE/DistortionFiles/miche_test_distortions_fce_stats.txt /home/vavieira/UnB/TCC/IrisDatabases/MICHE/DistortionFiles/miche_test_distortions_fce.txt 0.71
 
 # ubirisv1
-python ../Python/compare_file_measures.py ~/UnB/TCC/IrisDatabases/UBIRISv1/ubirisv1_test_fce_stats.txt /home/vavieira/UnB/TCC/IrisDatabases/UBIRISv1/ubirisv1_test_fce.txt 0.71
+python ../../Python/compare_file_measures.py ~/UnB/TCC/IrisDatabases/UBIRISv1/DistortionFiles/ubirisv1_test_distortions_fce_stats.txt /home/vavieira/UnB/TCC/IrisDatabases/UBIRISv1/DistortionFiles/ubirisv1_test_distortions_fce.txt 0.71
 
 # ubirisv2
-python ../Python/compare_file_measures.py ~/UnB/TCC/IrisDatabases/UBIRISv2/ubirisv2_test_fce_stats.txt /home/vavieira/UnB/TCC/IrisDatabases/UBIRISv2/ubirisv2_test_fce.txt 0.71
+python ../../Python/compare_file_measures.py ~/UnB/TCC/IrisDatabases/UBIRISv2/DistortionFiles/ubirisv2_test_distortions_fce_stats.txt /home/vavieira/UnB/TCC/IrisDatabases/UBIRISv2/DistortionFiles/ubirisv2_test_distortions_fce.txt 0.71
 
 ##################################################################################################################
 
 ################################COMBINING ORIGINAL IMAGES WITH DISTORTIONS########################################
+echo "Combining original images with distortions"
 #warsaw
 cat /home/vavieira/UnB/TCC/IrisDatabases/Warsaw-BioBase-Smartphone-Iris-v1.0/NoDistortionFiles/warsaw_test_fce.txt > /home/vavieira/UnB/TCC/IrisDatabases/Warsaw-BioBase-Smartphone-Iris-v1.0/DistortionFiles/warsaw_test_all_fce.txt
 cat /home/vavieira/UnB/TCC/IrisDatabases/Warsaw-BioBase-Smartphone-Iris-v1.0/DistortionFiles/warsaw_test_distortions_fce.txt >> /home/vavieira/UnB/TCC/IrisDatabases/Warsaw-BioBase-Smartphone-Iris-v1.0/DistortionFiles/warsaw_test_all_fce.txt
 
 # miche
 cat /home/vavieira/UnB/TCC/IrisDatabases/MICHE/NoDistortionFiles/miche_test_fce.txt > /home/vavieira/UnB/TCC/IrisDatabases/MICHE/DistortionFiles/miche_test_all_fce.txt
-cat /home/vavieira/UnB/TCC/IrisDatabases/MICHE/DistortionFiles/miche_test_distortions_fce.txt >> /home/vavieira/UnB/TCC/IrisDatabases/DistortionFiles/MICHE/miche_test_all_fce.txt
+cat /home/vavieira/UnB/TCC/IrisDatabases/MICHE/DistortionFiles/miche_test_distortions_fce.txt >> /home/vavieira/UnB/TCC/IrisDatabases/MICHE/DistortionFiles/miche_test_all_fce.txt
 
 # ubirisv1
 cat /home/vavieira/UnB/TCC/IrisDatabases/UBIRISv1/NoDistortionFiles/ubirisv1_test_fce.txt > /home/vavieira/UnB/TCC/IrisDatabases/UBIRISv1/DistortionFiles/ubirisv1_test_all_fce.txt
@@ -40,17 +41,18 @@ cat /home/vavieira/UnB/TCC/IrisDatabases/UBIRISv2/DistortionFiles/ubirisv2_test_
 ##################################################################################################################
 
 ################################FILTERING AT LEAST TWO SUBJECTS ##################################################
+echo "Filtering images that only has subjects with at least 2 images"
 # Warsaw
-python ../Python/check_for_two_ocurrences.py /home/vavieira/UnB/TCC/IrisDatabases/Warsaw-BioBase-Smartphone-Iris-v1.0/DistortionFiles/ warsaw_test_all_fce.txt warsaw
+python ../../Python/check_for_two_ocurrences.py /home/vavieira/UnB/TCC/IrisDatabases/Warsaw-BioBase-Smartphone-Iris-v1.0/DistortionFiles/ warsaw_test_all_fce.txt warsaw
 
 # MICHE
-python ../Python/check_for_two_ocurrences.py /home/vavieira/UnB/TCC/IrisDatabases/MICHE/DistortionFiles/ miche_test_all_fce.txt miche
+python ../../Python/check_for_two_ocurrences.py /home/vavieira/UnB/TCC/IrisDatabases/MICHE/DistortionFiles/ miche_test_all_fce.txt miche
 
 # UBIRISv1
-python ../Python/check_for_two_ocurrences.py /home/vavieira/UnB/TCC/IrisDatabases/UBIRISv1/DistortionFiles/ ubirisv1_test_all_fce.txt ubirisv1
+python ../../Python/check_for_two_ocurrences.py /home/vavieira/UnB/TCC/IrisDatabases/UBIRISv1/DistortionFiles/ ubirisv1_test_all_fce.txt ubirisv1
 
 # UBIRISv2
-python ../Python/check_for_two_ocurrences.py /home/vavieira/UnB/TCC/IrisDatabases/UBIRISv2/DistortionFiles/ ubirisv2_test_all_fce.txt ubirisv2
+python ../../Python/check_for_two_ocurrences.py /home/vavieira/UnB/TCC/IrisDatabases/UBIRISv2/DistortionFiles/ ubirisv2_test_all_fce.txt ubirisv2
 
 
 ##################################################################################################################
@@ -58,37 +60,37 @@ python ../Python/check_for_two_ocurrences.py /home/vavieira/UnB/TCC/IrisDatabase
 ################################GENERATING MATCHING FILES#########################################################
 #Generating files with all the files that has a fce value higher than the computed threshold in order
 # to perform matching
-
+echo "Generating matching files"
 # warsaw
-python ../Python/generate_matching_file.py ~/UnB/TCC/IrisDatabases/Warsaw-BioBase-Smartphone-Iris-v1.0/DistortionFiles/warsaw_test_all_fce_filtered.txt /home/vavieira/UnB/TCC/IrisDatabases/Warsaw-BioBase-Smartphone-Iris-v1.0/DistortionFiles/warsaw_matching_test_all_fce_filtered.txt
+python ../../Python/generate_matching_file.py ~/UnB/TCC/IrisDatabases/Warsaw-BioBase-Smartphone-Iris-v1.0/DistortionFiles/warsaw_test_all_fce_filtered.txt /home/vavieira/UnB/TCC/IrisDatabases/Warsaw-BioBase-Smartphone-Iris-v1.0/DistortionFiles/warsaw_matching_test_all_fce_filtered.txt
 
 # miche
-python ../Python/generate_matching_file.py ~/UnB/TCC/IrisDatabases/MICHE/DistortionFiles/miche_test_all_fce_filtered.txt ~/UnB/TCC/IrisDatabases/MICHE/DistortionFiles/miche_matching_test_all_fce_filtered.txt
+python ../../Python/generate_matching_file.py ~/UnB/TCC/IrisDatabases/MICHE/DistortionFiles/miche_test_all_fce_filtered.txt ~/UnB/TCC/IrisDatabases/MICHE/DistortionFiles/miche_matching_test_all_fce_filtered.txt
 
 # ubirisv1
-python ../Python/generate_matching_file.py ~/UnB/TCC/IrisDatabases/UBIRISv1/DistortionFiles/ubirisv1_test_all_fce_filtered.txt ~/UnB/TCC/IrisDatabases/UBIRISv1/DistortionFiles/ubirisv1_matching_test_all_fce_filtered.txt
+python ../../Python/generate_matching_file.py ~/UnB/TCC/IrisDatabases/UBIRISv1/DistortionFiles/ubirisv1_test_all_fce_filtered.txt ~/UnB/TCC/IrisDatabases/UBIRISv1/DistortionFiles/ubirisv1_matching_test_all_fce_filtered.txt
 
 # ubirisv2
-python ../Python/generate_matching_file.py ~/UnB/TCC/IrisDatabases/UBIRISv2/DistortionFiles/ubirisv2_test_all_fce_filtered.txt ~/UnB/TCC/IrisDatabases/UBIRISv2/DistortionFiles/ubirisv2_matching_test_all_fce_filtered.txt
+python ../../Python/generate_matching_file.py ~/UnB/TCC/IrisDatabases/UBIRISv2/DistortionFiles/ubirisv2_test_all_fce_filtered.txt ~/UnB/TCC/IrisDatabases/UBIRISv2/DistortionFiles/ubirisv2_matching_test_all_fce_filtered.txt
 
 ##################################################################################################################
 
 ################################PERFORMING MATCHING#########################################################
 #Performing matching in every database
-
+echo "Peroforming matching"
 # Changing to the proper folder
 cd ~/UnB/TCC/IrisDatabases/Test-Osiris/
 
 # warsaw
-~/UnB/TCC/Codigos/Iris_Osiris/src/osiris ./Warsaw/Matching_FCE/
+~/UnB/TCC/Codigos/Iris_Osiris/src/osiris ./Warsaw/Matching_FCE_Distortions/
 
 # miche
-~/UnB/TCC/Codigos/Iris_Osiris/src/osiris ./MICHE/Matching_FCE/
+~/UnB/TCC/Codigos/Iris_Osiris/src/osiris ./MICHE/Matching_FCE_Distortions/
 
 # ubirisv1
-~/UnB/TCC/Codigos/Iris_Osiris/src/osiris ./UBIRISv1/Matching_FCE/
+~/UnB/TCC/Codigos/Iris_Osiris/src/osiris ./UBIRISv1/Matching_FCE_Distortions/
 
 # ubirisv2
-~/UnB/TCC/Codigos/Iris_Osiris/src/osiris ./UBIRISv2/Matching_FCE/
+~/UnB/TCC/Codigos/Iris_Osiris/src/osiris ./UBIRISv2/Matching_FCE_Distortions/
 
 ##################################################################################################################
